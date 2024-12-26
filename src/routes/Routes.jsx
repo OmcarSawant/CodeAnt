b7a5a4e7194bc5db0e7523f00746111457729513
OmcarSawant <123458301+OmcarSawant@users.noreply.github.com>
2024-12-26 21:53:00 +0530

    Add files via upload

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignIn from "../pages/SignIn";
+import Home from "../pages/Home"
+
+const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/signin", element: <SignIn /> }
+]);
+
+const Routes = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default Routes;
